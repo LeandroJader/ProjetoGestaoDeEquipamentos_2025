b33ef3d1f39c79ca48bf2f96bc05ac483dc91222
leandroJader <ldandro>
2025-04-03 14:42:51 -0300

    Add project files.

---
+++ ProjetoGestaoDeEquipamentos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoGestaoDeEquipamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
